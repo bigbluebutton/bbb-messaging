c3d9582bc5c028c5c79668eab766a3394b0a3912
Richard Alam <ritzalam@gmail.com>
2019-11-13 14:50:36 -0800

    - try to use vertx js

---
+++ src/senders/users_sender.ts
@@ -0,0 +1,12 @@
+import { Sender } from '../sender'
+
+export class UsersSender {
+    sender: Sender;
+    
+    constructor(sender: Sender) {
+        this.sender = sender;
+
+    }
+
+
+}
---
+++ src/vertx_conn.ts
@@ -0,0 +1,15 @@
+import { EventBus } from '../js3rdparty/vertx-eventbus'
+
+export class VertxConn {
+    vertx: any;
+
+    constructor(url: string, options={}) {
+        this.vertx = new EventBus(url, options)
+    }
+
+    private onclose(): void {
+        console.log("On Close");
+    }
+
+
+}
